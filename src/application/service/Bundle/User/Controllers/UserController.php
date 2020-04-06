83d5367610016a4cf409a420f14bf293ce507ca1
zekiunal <Bun@2081>
2020-04-07 01:35:27 +0300

    User controller created.

---
+++ src/application/service/Bundle/User/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Bundle\User\Controllers;
+
+use Foreweather\Phalcon\Http\Response;
+use Phalcon\Mvc\Controller;
+
+/**
+ * Class BaseController
+ *
+ * @property Response            $response
+ */
+class UserController extends Controller
+{
+
+}
